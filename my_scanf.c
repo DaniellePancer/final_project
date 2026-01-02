723f6833235172d27f8814ca1fd0030eb8ce245e
Temima Pancer <dpancer@mail.yu.edu>
2026-01-02 15:22:45 -0500

    Fixed bug

--- my_scanf.c
+++ my_scanf.c
@@ -2,7 +2,12 @@
 #include <stdarg.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <wchar.h> 
+#include <wchar.h>
+
+static FILE *input_file = NULL;
+
+#define getchar() (input_file ? fgetc(input_file) : fgetc(stdin))
+#define ungetc(c, f) (input_file ? ungetc(c, input_file) : ungetc(c, stdin))
 
 static void skip_spaces(void) {
     int c;
@@ -15,56 +20,43 @@ static void skip_spaces(void) {
 }
 
 static int read_numbers(long long *out, int base, int width, int signed_flag) {
-    int c;
+    int c, sign = 1, digits = 0;
     long long value = 0;
-    int sign = 1, digits = 0;
-
     skip_spaces();
     c = getchar();
-
-    if (signed_flag && c == '-') {
-        sign = -1;
-        c = getchar();
+    if (signed_flag && (c == '-' || c == '+')) { 
+        if (c == '-') sign = -1;
+        c = getchar(); 
     }
-
-    while (c != EOF && width != 0) {
+    while (c != EOF && (width != 0)) {
         int digit = -1;
         if (isdigit(c)) digit = c - '0';
         else if (isalpha(c)) digit = tolower(c) - 'a' + 10;
-
         if (digit < 0 || digit >= base) break;
-
         value = value * base + digit;
         digits++;
-        width--;
+        if (width > 0) width--;
         c = getchar();
     }
-
     if (c != EOF) ungetc(c, stdin);
     if (digits == 0) return 0;
-
     *out = value * sign;
     return 1;
 }
 
 static int read_unsigned(unsigned long long *out, int width) {
-    int c;
+    int c, digits = 0;
     unsigned long long value = 0;
-    int digits = 0;
-
     skip_spaces();
     c = getchar();
-
-    while (isdigit(c) && width != 0) {
+    while (isdigit(c) && (width != 0)) {
         value = value * 10 + (c - '0');
         digits++;
-        width--;
+        if (width > 0) width--;
         c = getchar();
     }
-
     if (c != EOF) ungetc(c, stdin);
     if (digits == 0) return 0;
-
     *out = value;
     return 1;
 }
@@ -72,32 +64,21 @@ static int read_unsigned(unsigned long long *out, int width) {
 static int read_floats(long double *out, int width) {
     int c, sign = 1, seen_dot = 0;
     long double value = 0.0, divisor = 10.0;
-
     skip_spaces();
     c = getchar();
-
-    if (c == '-') {
-        sign = -1;
-        c = getchar();
-    }
-
+    if (c == '-' || c == '+') { if (c == '-') sign = -1; c = getchar(); }
     if (!isdigit(c) && c != '.') return 0;
-
-    while ((isdigit(c) || c == '.') && width != 0) {
+    while ((isdigit(c) || c == '.') && (width != 0)) {
         if (c == '.') {
             if (seen_dot) break;
             seen_dot = 1;
         } else {
             if (!seen_dot) value = value * 10 + (c - '0');
-            else {
-                value += (c - '0') / divisor;
-                divisor *= 10.0;
-            }
+            else { value += (c - '0') / divisor; divisor *= 10.0; }
         }
-        width--;
+        if (width > 0) width--;
         c = getchar();
     }
-
     if (c != EOF) ungetc(c, stdin);
     *out = value * sign;
     return 1;
@@ -105,19 +86,15 @@ static int read_floats(long double *out, int width) {
 
 static int read_strings(char *out, int width) {
     int c, count = 0;
-
     skip_spaces();
     c = getchar();
-
-    while (c != EOF && !isspace(c) && width != 0) {
+    while (c != EOF && !isspace(c) && (width != 0)) {
         out[count++] = c;
-        width--;
+        if (width > 0) width--;
         c = getchar();
     }
-
     if (c != EOF) ungetc(c, stdin);
     out[count] = '\0';
-
     return count > 0;
 }
 
@@ -130,16 +107,13 @@ static int read_characters(char *out) {
 
 static int read_quote_strings(char *out, int width) {
     int c, count = 0;
-
     skip_spaces();
     c = getchar();
     if (c != '"') return 0;
-
-    while ((c = getchar()) != EOF && c != '"' && width != 0) {
+    while ((c = getchar()) != EOF && c != '"' && (width != 0)) {
         out[count++] = c;
-        width--;
+        if (width > 0) width--;
     }
-
     out[count] = '\0';
     return 1;
 }
@@ -147,77 +121,69 @@ static int read_quote_strings(char *out, int width) {
 int my_scanf(const char *format, ...) {
     va_list parameters;
     va_start(parameters, format);
-
     int assigned = 0;
 
     while (*format) {
         if (*format == '%') {
             format++;
+            int width = -1;
+            int length = 0;
+            if (isdigit(*format)) { width = 0; while (isdigit(*format)) { width = width*10 + (*format-'0'); format++; } }
 
-            int width = 0;
-            int length = 0; 
-
-            while (isdigit(*format)) {
-                width = width * 10 + (*format - '0');
-                format++;
-            }
-            if (width == 0) width = -1;
-
-            
-            if (*format == 'h') {
-                format++;
-                if (*format == 'h') { length = 2; format++; } 
-                else length = 1;
-            } else if (*format == 'l') {
-                format++;
-                if (*format == 'l') { length = 4; format++; } 
-                else length = 3;
-            } else if (*format == 'L') { length = 5; format++; }
+            if (*format == 'h') { format++; if (*format == 'h') { length=2; format++; } else length=1; }
+            else if (*format == 'l') { format++; if (*format == 'l') { length=4; format++; } else length=3; }
+            else if (*format == 'L') { length=5; format++; }
 
             switch (*format) {
-                case 'd': {
+                case 'd': case 'i': {
                     long long temp;
-                    if (!read_numbers(&temp, 10, width, 1)) goto done;
-                    if (length == 0) { int *p = va_arg(parameters,int*); *p=(int)temp; }
-                    else if (length == 1) { short *p = va_arg(parameters,short*); *p=(short)temp; }
-                    else if (length == 2) { signed char *p=va_arg(parameters,signed char*); *p=(signed char)temp; }
-                    else if (length == 3) { long *p=va_arg(parameters,long*); *p=(long)temp; }
-                    else if (length == 4) { long long *p=va_arg(parameters,long long*); *p=temp; }
+                    if (!read_numbers(&temp,10,width,1)) goto done;
+                    if (length==0) { int *p=va_arg(parameters,int*); *p=(int)temp; }
+                    else if (length==1) { short *p=va_arg(parameters,short*); *p=(short)temp; }
+                    else if (length==2) { signed char *p=va_arg(parameters,signed char*); *p=(signed char)temp; }
+                    else if (length==3) { long *p=va_arg(parameters,long*); *p=(long)temp; }
+                    else if (length==4) { long long *p=va_arg(parameters,long long*); *p=temp; }
                     assigned++; break;
                 }
                 case 'u': {
                     unsigned long long temp;
-                    if (!read_unsigned(&temp, width)) goto done;
-                    if (length == 0) { unsigned *p = va_arg(parameters,unsigned*); *p=(unsigned)temp; }
-                    else if (length == 1) { unsigned short *p=va_arg(parameters,unsigned short*); *p=(unsigned short)temp; }
-                    else if (length == 2) { unsigned char *p=va_arg(parameters,unsigned char*); *p=(unsigned char)temp; }
-                    else if (length == 3) { unsigned long *p=va_arg(parameters,unsigned long*); *p=(unsigned long)temp; }
-                    else if (length == 4) { unsigned long long *p=va_arg(parameters,unsigned long long*); *p=temp; }
+                    if (!read_unsigned(&temp,width)) goto done;
+                    if (length==0) { unsigned *p=va_arg(parameters,unsigned*); *p=(unsigned)temp; }
+                    else if (length==1) { unsigned short *p=va_arg(parameters,unsigned short*); *p=(unsigned short)temp; }
+                    else if (length==2) { unsigned char *p=va_arg(parameters,unsigned char*); *p=(unsigned char)temp; }
+                    else if (length==3) { unsigned long *p=va_arg(parameters,unsigned long*); *p=(unsigned long)temp; }
+                    else if (length==4) { unsigned long long *p=va_arg(parameters,unsigned long long*); *p=temp; }
                     assigned++; break;
                 }
                 case 'x': {
                     unsigned long long temp;
                     if (!read_numbers((long long*)&temp,16,width,0)) goto done;
-                    if (length == 0) { unsigned *p=va_arg(parameters,unsigned*); *p=(unsigned)temp; }
-                    else if (length == 1) { unsigned short *p=va_arg(parameters,unsigned short*); *p=(unsigned short)temp; }
-                    else if (length == 2) { unsigned char *p=va_arg(parameters,unsigned char*); *p=(unsigned char)temp; }
-                    else if (length == 3) { unsigned long *p=va_arg(parameters,unsigned long*); *p=(unsigned long)temp; }
-                    else if (length == 4) { unsigned long long *p=va_arg(parameters,unsigned long long*); *p=temp; }
+                    if (length==0) { unsigned *p=va_arg(parameters,unsigned*); *p=(unsigned)temp; }
+                    else if (length==1) { unsigned short *p=va_arg(parameters,unsigned short*); *p=(unsigned short)temp; }
+                    else if (length==2) { unsigned char *p=va_arg(parameters,unsigned char*); *p=(unsigned char)temp; }
+                    else if (length==3) { unsigned long *p=va_arg(parameters,unsigned long*); *p=(unsigned long)temp; }
+                    else if (length==4) { unsigned long long *p=va_arg(parameters,unsigned long long*); *p=temp; }
                     assigned++; break;
+                }
+                case 'b': {
+                    long long temp;
+                    if (!read_numbers(&temp,2,width,0)) goto done;
+                    int *p=va_arg(parameters,int*);
+		    *p=temp;
+                    assigned++;
+		    break;
                 }
                 case 'f': {
                     long double temp;
                     if (!read_floats(&temp,width)) goto done;
-                    if (length == 0) { float *p=va_arg(parameters,float*); *p=(float)temp; }
-                    else if (length == 3) { double *p=va_arg(parameters,double*); *p=(double)temp; }
-                    else if (length == 5) { long double *p=va_arg(parameters,long double*); *p=temp; }
+                    if (length==0) { float *p=va_arg(parameters,float*); *p=(float)temp; }
+                    else if (length==3) { double *p=va_arg(parameters,double*); *p=(double)temp; }
+                    else if (length==5) { long double *p=va_arg(parameters,long double*); *p=temp; }
                     assigned++; break;
                 }
                 case 's': {
-                    if (length == 0) { char *p=va_arg(parameters,char*); if(!read_strings(p,width)) goto done; }
-                    else if (length == 3) { 
-                        wchar_t *p=va_arg(parameters,wchar_t*);
-                        int c,count=0;
+                    if (length==0) { char *p=va_arg(parameters,char*); if(!read_strings(p,width)) goto done; }
+                    else if (length==3) { wchar_t *p=va_arg(parameters,wchar_t*); int c,count=0;
                         skip_spaces();
                         while((c=getchar())!=EOF && !isspace(c) && (width!=0)) {
                             p[count++]=(wchar_t)c;
@@ -229,7 +195,7 @@ int my_scanf(const char *format, ...) {
                     assigned++; break;
                 }
                 case 'c': {
-                    if (length==0) { char *p=va_arg(parameters,char*); if(!read_characters(p)) goto done; }
+                    if(length==0) { char *p=va_arg(parameters,char*); if(!read_characters(p)) goto done; }
                     else if(length==3) { wchar_t *p=va_arg(parameters,wchar_t*); int c=getchar(); if(c==EOF) goto done; *p=(wchar_t)c; }
                     assigned++; break;
                 }
@@ -238,6 +204,7 @@ int my_scanf(const char *format, ...) {
                 }
                 default: goto done;
             }
+
         } else if (isspace(*format)) {
             skip_spaces();
         } else {
--- test_scanf.c
+++ test_scanf.c
@@ -3,147 +3,166 @@
 #include <assert.h>
 #include <stdlib.h>
 
-
 #include "my_scanf.c"
 
 void run_tests() {
-    // ----- Integer test -----
+
     {
         int result = 0;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "42\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%d", &result);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(result == 42);
         fclose(input_stream);
     }
 
-    // ----- Hex test -----
+
     {
         int result = 0;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "1a\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%x", &result);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(result == 26);
         fclose(input_stream);
     }
 
-    // ----- Binary test -----
+
     {
         int result = 0;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "1010\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%b", &result);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(result == 10);
         fclose(input_stream);
     }
 
-    // ----- Unsigned test -----
+
     {
         unsigned u = 0;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "1234\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%u", &u);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(u == 1234);
         fclose(input_stream);
     }
 
-    // ----- Float test -----
+
     {
         float f = 0.0f;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "-12.34\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%f", &f);
+        input_file = NULL;
+
         assert(assigned == 1);
-        assert(f < -12.33f && f > -12.35f);  // allow tiny float error
+        assert(f < -12.33f && f > -12.35f);
         fclose(input_stream);
     }
 
-    // ----- String test -----
+
     {
         char s[20];
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "hello\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%s", s);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(strcmp(s, "hello") == 0);
         fclose(input_stream);
     }
 
-    // ----- Character test -----
+
     {
         char c;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "K\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%c", &c);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(c == 'K');
         fclose(input_stream);
     }
 
-    // ----- Quoted string test -----
+
     {
         char q[50];
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "\"first quote\"\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%q", q);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(strcmp(q, "first quote") == 0);
         fclose(input_stream);
     }
 
-    // ----- Edge cases -----
+
     {
         int result;
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "   -99  ");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%d", &result);
+        input_file = NULL;
+
         assert(assigned == 1);
         assert(result == -99);
         fclose(input_stream);
     }
 
-    // ----- Width-limited string -----
+
     {
         char s[5];
         FILE *input_stream = tmpfile();
         fprintf(input_stream, "HelloWorld\n");
         rewind(input_stream);
-        stdin = input_stream;
 
+        input_file = input_stream;
         int assigned = my_scanf("%4s", s);
+        input_file = NULL;
+
         assert(assigned == 1);
-        assert(strcmp(s, "Hell") == 0);  // only 4 chars
+        assert(strcmp(s, "Hell") == 0);
         fclose(input_stream);
     }
 }
